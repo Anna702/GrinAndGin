1ae76107c11f81f74e1a0f79c928994aa8e437ed
Toni97 <ayovcheva75@gmail.com>
2024-01-18 20:15:44 +0000

    Algorithm for js code

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+// Check if localStorage already has search history and liked recipes- initiate when rendering
+
+// Function to render search history and liked recipes
+   // Code to render search history and liked recipes on the webpage
+
+// Function to fetch cocktail data from CocktailDB API
+   // Code to make API request to CocktailDB based on ingredient and non-alcoholic/Alcoholic
+    // Parse and return relevant cocktail data
+
+// Function to fetch food suggestion from a second API 
+// Code to make API request for food suggestion
+    // Parse and return relevant food data
+
+// Function to display results on the webpage
+
+// Event listener for the search button
+   // Research async/await
+   // Fetch cocktail data and food suggestion
+   // Display results on the webpage
+   // Save search to search history 
+   // Render updated search history on the webpage
+
+// Event listener for like/dislike buttons
+   // Code to handle liked recipes
+    // Update liked recipes array and save to local storage
+    // Render updated liked recipes on the webpage
+    // Code to handle disliking a recipe
